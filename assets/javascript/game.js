85beda02ef76f35effc25e66b78ecad556831966
Sean Leif Hellier <sean@newtropolis.com>
2019-08-30 18:29:56 -0400

    established game functionality (with great help from tutor Benjamin Benson. Hat's off to that dude

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-        var $ = function (id)
-    {
-        return document.getElementById(id);
-    } 
-     
+var $$ = function(id) {
+  return document.getElementById(id);
+};
 
 // declare variables to initialize array and generate random pick from the array
 
@@ -10,169 +8,98 @@
 // 2. set how many guesses are allowed
 // 3. Initialize the list of letters are in the word and present placeholders
 // 4. get the number of letters are in the chosen word
-        var game = ["APOLLO100", "BEETHOVEN", "MOZART", "KLF"]
-        var choice = Math.floor(Math.random()*4);
-        var answer = "";
-        // var myLength = answer.length;
-        // var display = [myLength];
-        // var win = myLength;
-        var letters = [];
-        var attemptsLeft = 10;
-        var userLetter="";
-        var wrongGuess="";
-        // console.log('answer ', answer);
+var gameActive = true;
+var answer = "";
+var letters = [];
+var attemptsLeft = 10;
+var userGuess = "";
+var wrongGuess = "";
+var wins = 0,
+  losses = 0,
+  resetText = "";
 
-    
-    function assignAnswer() {
-        answer = game[choice] 
-        // letters = answer.split('');
-        console.log("This is the assigned Answer: " + answer)
-        // console.log("this should be an array with all the letters in answer: "+ letters)
+function startGame() {
+  var game = ["APOLLO100", "BEETHOVEN", "MOZART", "KLF"];
+  var choice = Math.floor(Math.random() * game.length);
+  answer = game[choice];
+  attemptsLeft = 10;
+  $$("attemptsLeft").innerHTML = attemptsLeft;
+  CreateLetters();
+  console.log("This is the assigned Answer: " + answer);
+}
+// function that puts blanks on page
 
+function CreateLetters() {
+  letters = [];
+  for (let i = 0; i < answer.length; i++) {
+    letters.push("_");
+  }
+  displayLetters();
+}
+
+function displayLetters() {
+  $$("game").innerHTML = letters.join(" ");
+}
+
+function checkGuess(guess) {
+  var foundMatch = false;
+  // this loop I want to run over whatever the answer word is
+  for (var i = 0; i < answer.length; i++) {
+    // for the if/else statement, IF userLetter is in "answer" we want to replace the __ on the screen
+    if (guess === answer[i]) {
+      foundMatch = true;
+      letters[i] = guess;
+      displayLetters();
     }
-
-    assignAnswer()
-
-    // function that puts blanks on page
-    for (let i = 0; i < answer.length; i++) {
-        letters.push("_");
-        console.log()
-    }
-
-    var setup = function()
-    {
-        var output="";
-        // console.log('working with ', answer);
-        // console.log('letters ', letters);
-        for (var i=0; i< answer.length; i++)
-        {
-            output = output + letters[i] + " ";
-        }
-        document.getElementById("game").innerHTML = output;
-        // console.log('output ', output)
-    }
-
-    function checkGuess(userLetter) {
-        
-        // this loop I want to run over whatever the answer word is
-        for (var i = 0; i < answer.length; i++) {
-           
-            // for the if/else statement, IF userLetter is in "answer" we want to replace the __ on the screen 
-            if (userLetter === answer[i]){
-                letters[i] = userLetter;
-            }
-        }
-        console.log('answer is ', letters);
-        setup();
-    }
-
-    // updateGuesses: function(letter) {
-    //     // If the letter is not in the guessedLetters array, and the letter is not in the lettersOfTheWord array..
-    //     if ((this.guessedLetters.indexOf(letter) === -1) && (this.lettersOfTheWord.indexOf(letter) === -1)) {
-    
-    //       // Add the letter to the guessedLetters array.
-    //       this.guessedLetters.push(letter);
-    
-    //       // Decrease guesses by one.
-    //       this.guessesLeft--;
-    
-    //       // Update guesses remaining and guesses letters on the page.
-    //       document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
-    //       document.querySelector("#guessed-letters").innerHTML =
-    //       this.guessedLetters.join(", ");
-    //     }
-
-
-
-
-
-    var submit = function()
-    {
-        output = "";
-        $("letter").value(userLetter);
-        $("letter").value = "";
-
-
-        for (var c= 0; c<answer.length; c++)
-        {
-            alert(letters[c]);
-            if (userLetter.toUpperCase() == letters[c])
-                {
-                    display[c] = userLetter.toUpperCase();
-                    win--;
-                }
-                    output = output + display[c] + " ";
-        }
-
-
-        // When user chooses letter, check to see if it's in the chosen word string (done).
-        // if yes, put it in appropriate place (done)
-        // if not found, put in not found string and display in appropriate place / decrement number of guesses remaining
-        
-        // if number of correctly guessed letters matches all of chosen word String,
-
-        // declare win / play song
-
-        // else, continue the game until either guessed letter string matches chosen string or number of guesses remaining reaches zero.
-
-// This function is run whenever the user guesses a letter..
-gameProcess = function() {
-    // If the user has no guesses left, restart the game.
-    if (this.attemptsLeft === 0) {
-      this.restartGame();
-    }
-    // Otherwise...
-    else {
-      // Check for and handle incorrect guesses.
-      this.wrongGuess(userLetter);
-
-      // Check for and handle correct guesses.
-      this.updateMatchedLetters(userLetter);
-
-      // If the user wins, restart the game.
-      if (this.updateWins() === true) {
-        this.restartGame();
-      }
-    }
-
   }
 
-  gameProcess();
-
-
-
-
-
-
-        // OLD CODE HERE
-        // document.getElementById("game").innerHTML = output;
-        // output="";
-        // attemptsLeft--;
-
-        // if (win < 1)
-        // {
-        //     document.getElementById("gusses").innerHTML = "You win!";
-        // }
-        // else if (attemptsLeft < 1)
-        //     {
-        //         document.getElementById("gusses").innerHTML = "You Lose!";
-        //     }
-        // else
-        //     {
-        //     document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + "guesses left";
-        //  }
+  if (foundMatch) {
+    console.log("You found a match");
+    if (!letters.includes("_")) {
+      resetText = "You have won!";
+      wins++;
+      $$("wins").innerHTML = wins;
+      gameActive = false;
     }
-  
-    window.onload = function()
-    {
-        setup();
-        // $("submit").onclick = submit;
-
-        document.onkeyup = function() {
-            const userGuess = event.key.toUpperCase();
-            checkGuess(userGuess);
-            console.log('userGuess', userGuess)
-        }
+  } else {
+    attemptsLeft--;
+    if (attemptsLeft === 0) {
+      resetText = "you have lost!";
+      losses++;
+      $$("losses").innerHTML = losses;
+      gameActive = false;
     }
-  
+    console.log(attemptsLeft);
+    $$("attemptsLeft").innerHTML = attemptsLeft;
+  }
+  console.log("answer is ", letters);
+}
 
+// When user chooses letter, check to see if it's in the chosen word string (done).
+// if yes, put it in appropriate place (done)
+// if not found, put in not found string and display in appropriate place / decrement number of guesses remaining
+
+// if number of correctly guessed letters matches all of chosen word String,
+
+// declare win / play song
+
+// else, continue the game until either guessed letter string matches chosen string or number of guesses remaining reaches zero.
+
+// This function is run whenever the user guesses a letter..
+
+window.onload = function() {
+  startGame();
+  document.onkeyup = function(event) {
+    const userGuess = event.key.toUpperCase();
+    if (gameActive) {
+      checkGuess(userGuess);
+    } else {
+      var reset = confirm(resetText);
+      if (reset || true) {
+        resetText = "";
+        gameActive = true;
+        startGame();
+      }
+    }
+  };
+};
